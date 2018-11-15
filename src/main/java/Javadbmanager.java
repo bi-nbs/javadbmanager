4e8c894c34deeb868f68a0ab4ae5648dd486029d
Niels Bækgaard-Sørensen <nbs@baekgaardit.dk>
2018-11-15 19:16:13 +0100

    Sejt
    Sejt

--- src/main/java/Javadbmanager.java
+++ src/main/java/Javadbmanager.java
@@ -1,2 +1,15 @@
+import Properties.PropertiesShop;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Javadbmanager {
+
+
+
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger();
+        logger.info(PropertiesShop.getProperties().getProperty("mysqlusername"));
+    }
+
 }
